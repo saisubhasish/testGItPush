eaf4686392e3a02b44767a58a96be54900089ce3
Sai <saisubhasishrout777@gmail.com>
2022-08-06 13:43:56 +0530

    This is my mongo file

--- mongotest.py
+++ mongotest.py
@@ -1,29 +1,13 @@
 import pymongo
 
-client = pymongo.MongoClient("mongodb+srv://sai1118:Sai1234@cluster0.d8m7g8f.mongodb.net/?retryWrites=true&w=majority")
+
+client = pymongo.MongoClient("mongodb://sai1118:Sai1234@ac-hfsjren-shard-00-00.d8m7g8f.mongodb.net:27017,ac-hfsjren-shard-00-01.d8m7g8f.mongodb.net:27017,ac-hfsjren-shard-00-02.d8m7g8f.mongodb.net:27017/?ssl=true&replicaSet=atlas-z7p8m3-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = client.test
-print(db)
-
 d = {
-    "name" : 'Sai',
-    "email" : 'sai@gmail.com',
-    "surname" : 'Rout'
-}
-
-db1 = client['mongotest']
-coll = db1['test']
+     'name' : 'Sai',
+     'email' : 'sai@gmail.com',
+     'surname' : 'Rout'
+ }
+db2 = client['mongotest']
+coll = db2['test']
 coll.insert_one(d)
-
-d = {
-    "name" : 'Sai',
-    "email" : 'sai@gmail.com',
-    "surname" : 'Rout',
-    'friends' : ['Sid','Somu','Kanhu']
-}
-
-d = {
-    "name" : 'Sai',
-    "email" : 'sai@gmail.com',
-    "surname" : 'Rout'
-}
-
